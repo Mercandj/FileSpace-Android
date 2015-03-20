15b1b694ed0b4ba459181432a12f38f625c42a10
Jonathan <modjow.jm9@gmail.com>
2015-03-21 00:22:44 +0100

    Add timer to the server almost ready

--- app/src/main/java/mercandalli/com/jarvis/dialog/DialogAddFileManager.java
+++ app/src/main/java/mercandalli/com/jarvis/dialog/DialogAddFileManager.java
@@ -10,14 +10,19 @@ import android.app.Dialog;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import mercandalli.com.jarvis.R;
 import mercandalli.com.jarvis.activity.Application;
@@ -40,7 +45,7 @@ public class DialogAddFileManager extends Dialog {
 		this.setContentView(R.layout.view_add_file);
 		this.setCancelable(true);
 
-        Animation animOpen = AnimationUtils.loadAnimation(this.app, R.anim.circle_button_bottom_open);
+        Animation animOpen = AnimationUtils.loadAnimation(this.app, R.anim.dialog_add_file_open);
         ((RelativeLayout) this.findViewById(R.id.relativeLayout)).startAnimation(animOpen);
 
         ((RelativeLayout) this.findViewById(R.id.relativeLayout)).setOnClickListener(new View.OnClickListener() {
@@ -81,7 +86,7 @@ public class DialogAddFileManager extends Dialog {
             }
         });
 
-        ((ImageButton) this.findViewById(R.id.txtFile)).setOnClickListener(new View.OnClickListener() {
+        ((RelativeLayout) this.findViewById(R.id.txtFile)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 app.prompt("Create TXT File", "Name ?", "Create", new IStringListener() {
@@ -93,6 +98,46 @@ public class DialogAddFileManager extends Dialog {
                 DialogAddFileManager.this.dismiss();
             }
         });
+
+        ((RelativeLayout) this.findViewById(R.id.addTimer)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                app.prompt("Create Timer File", "Content ?", "Create", new IStringListener() {
+                    @Override
+                    public void execute(String text) {
+
+                        TimeZone tz = TimeZone.getTimeZone("UTC");
+                        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH-mm'Z'");
+                        df.setTimeZone(tz);
+                        String nowAsISO = df.format(new Date());
+
+                        JSONObject json = new JSONObject();
+                        try {
+                            json.put("date", text);
+                            json.put("dateUpload", nowAsISO);
+
+                            List<BasicNameValuePair> parameters = new ArrayList<BasicNameValuePair>();
+                            parameters.add(new BasicNameValuePair("content",json.toString()));
+                            parameters.add(new BasicNameValuePair("name","TIMER_"+nowAsISO));
+                            new TaskPost(DialogAddFileManager.this.app,
+                                app.getConfig().getUrlServer()+app.getConfig().routeFile,
+                                new IPostExecuteListener() {
+                                    @Override
+                                    public void execute(JSONObject json, String body) {
+                                        if(listener!=null)
+                                            listener.execute(json, body);
+                                    }
+                                }
+                                ,parameters).execute();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, "Cancel", null);
+                DialogAddFileManager.this.dismiss();
+            }
+        });
+
         
         DialogAddFileManager.this.show();
 	}
