eb72954aab0b416bb639e791eb30375885602caf
Jonathan <modjow.jm9@gmail.com>
2015-03-23 21:34:32 +0100

    [Files] Add vcf

--- app/src/main/java/mercandalli/com/jarvis/model/ModelFileTypeENUM.java
+++ app/src/main/java/mercandalli/com/jarvis/model/ModelFileTypeENUM.java
@@ -8,7 +8,7 @@ package mercandalli.com.jarvis.model;
 
 public enum ModelFileTypeENUM {
 	APK(new ModelFileType("Android application", "apk")),
-	TEXT(new ModelFileType("Text", new String[] {"txt", "doc", "csv", "rtf", "text", "json"})),
+	TEXT(new ModelFileType("Text", new String[] {"txt", "csv", "rtf", "text", "json"})),
 	HTML(new ModelFileType("Html", new String[] {"html", "htm", "php", "xml"})),
 	PICTURE(new ModelFileType("Picture", new String[] {"jpeg",	"jpg", "png", "gif", "raw",	"psd", "bmp", "tiff", "tif"})),
 	AUDIO(new ModelFileType("Audio", new String[] {"mp3", "wav", "m4a", "aiff", "wma", "caf", "flac", "m4p", "amr"})),
@@ -16,8 +16,9 @@ public enum ModelFileTypeENUM {
 	ZIP(new ModelFileType("Zip", "zip")),
 	GZIP(new ModelFileType("Gzip", new String[] {"gzip", "rar", "tar", "tar.gz", "gz"})),
     JARVIS(new ModelFileType("Jarvis", new String[] {"jarvis"})),
-    WORD(new ModelFileType("Word", new String[] {"docx"})),
+    WORD(new ModelFileType("Word", new String[] {"doc", "docx"})),
     EXCEL(new ModelFileType("Excel", new String[] {"xlsx"})),
+    VCF(new ModelFileType("Business card", new String[] {"vcf"})),
     PDF(new ModelFileType("Pdf","pdf"));
 	
 	public ModelFileType type;
