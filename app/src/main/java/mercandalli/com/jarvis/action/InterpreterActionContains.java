7347b501ee7aec2a3184b45515dbf7a84ff63bed
Mercandj <modjow.jm9@gmail.com>
2015-05-14 18:05:01 +0100

    Update InterpreterActionContains.java

--- app/src/main/java/mercandalli/com/jarvis/action/InterpreterActionContains.java
+++ app/src/main/java/mercandalli/com/jarvis/action/InterpreterActionContains.java
@@ -1,3 +1,22 @@
+/**
+ * This file is part of Jarvis for Android, an app for managing your server (files, talks...).
+ *
+ * Copyright (c) 2014-2015 Jarvis for Android contributors (http://mercandalli.com)
+ *
+ * LICENSE:
+ *
+ * Jarvis for Android is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * Jarvis for Android is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * @author Jonathan Mercandalli
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2014-2015 Jarvis for Android contributors (http://mercandalli.com)
+ */
 package mercandalli.com.jarvis.action;
 
 import mercandalli.com.jarvis.activity.Application;
@@ -17,6 +36,13 @@ public class InterpreterActionContains extends Interpreter {
 
         if(input.contains("quit") || input.contains("kit l'app"))
             output = ENUM_Action.QUIT.action.action(this.app, "Bye.");
+            
+        else if((input.contains("mail") && input.contains("envo")) || input.equals("mail") || input.equals("email")) {
+            //TODO display item on home fragment with pre-completed edittext
+            //and SEND and DISMISS button
+            
+            output = "Fonctionnalité en developpement.";
+        }
 
         return output;
     }
