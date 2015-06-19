e5548485938e5fdabe2a33c8353814a348c3f710
Jonathan <modjow.jm9@gmail.com>
2015-06-19 22:47:40 +0200

    Update comments

--- app/src/main/java/mercandalli/com/filespace/util/ColorUtils.java
+++ app/src/main/java/mercandalli/com/filespace/util/ColorUtils.java
@@ -1,5 +1,23 @@
 package mercandalli.com.filespace.util;
-
+/**
+ * This file is part of FileSpace for Android, an app for managing your server (files, talks...).
+ *
+ * Copyright (c) 2014-2015 FileSpace for Android contributors (http://mercandalli.com)
+ *
+ * LICENSE:
+ *
+ * FileSpace for Android is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * FileSpace for Android is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * @author Jonathan Mercandalli
+ * @license http://www.gnu.org/licenses/gpl.html
+ * @copyright 2014-2015 FileSpace for Android contributors (http://mercandalli.com)
+ */
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.support.v7.graphics.Palette;
