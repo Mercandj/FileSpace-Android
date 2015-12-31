8a56a8c875bb2a70fe5217c63fe0e9aaac72862b
Mercandj <modjow.jm9@gmail.com>
2015-12-31 16:30:04 +0100

    Minor

--- filespace/src/main/java/com/mercandalli/android/apps/files/main/FileApp.java
+++ filespace/src/main/java/com/mercandalli/android/apps/files/main/FileApp.java
@@ -2,6 +2,8 @@ package com.mercandalli.android.apps.files.main;
 
 import android.app.Application;
 import android.content.Context;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class FileApp extends Application {
 
@@ -10,6 +12,7 @@ public class FileApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         //Dagger - Object graph creation
         setupGraph();
