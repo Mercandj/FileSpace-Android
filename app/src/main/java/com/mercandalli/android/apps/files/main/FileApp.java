4614b77f5af7a1f5da987397fb6001048480881f
Mercandj <modjow.jm9@gmail.com>
2016-01-08 19:02:10 +0100

    Analytics

--- app/src/main/java/com/mercandalli/android/apps/files/main/FileApp.java
+++ app/src/main/java/com/mercandalli/android/apps/files/main/FileApp.java
@@ -2,7 +2,14 @@ package com.mercandalli.android.apps.files.main;
 
 import android.app.Application;
 import android.content.Context;
+
 import com.crashlytics.android.Crashlytics;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.StandardExceptionParser;
+import com.google.android.gms.analytics.Tracker;
+import com.mercandalli.android.apps.files.analytics.AnalyticsTrackers;
+
 import io.fabric.sdk.android.Fabric;
 
 public class FileApp extends Application {
@@ -16,6 +23,9 @@ public class FileApp extends Application {
 
         //Dagger - Object graph creation
         setupGraph();
+
+        AnalyticsTrackers.initialize(this);
+        AnalyticsTrackers.getInstance().get(AnalyticsTrackers.Target.APP);
     }
 
     private void setupGraph() {
@@ -33,4 +43,62 @@ public class FileApp extends Application {
     public static FileApp get(Context context) {
         return (FileApp) context.getApplicationContext();
     }
+
+
+    // region Analytics
+    public synchronized Tracker getGoogleAnalyticsTracker() {
+        AnalyticsTrackers analyticsTrackers = AnalyticsTrackers.getInstance();
+        return analyticsTrackers.get(AnalyticsTrackers.Target.APP);
+    }
+
+    /***
+     * Tracking screen view
+     *
+     * @param screenName screen name to be displayed on GA dashboard
+     */
+    public void trackScreenView(String screenName) {
+        Tracker t = getGoogleAnalyticsTracker();
+
+        // Set screen name.
+        t.setScreenName(screenName);
+
+        // Send a screen view.
+        t.send(new HitBuilders.ScreenViewBuilder().build());
+
+        GoogleAnalytics.getInstance(this).dispatchLocalHits();
+    }
+
+    /***
+     * Tracking exception
+     *
+     * @param e exception to be tracked
+     */
+    public void trackException(Exception e) {
+        if (e != null) {
+            Tracker t = getGoogleAnalyticsTracker();
+
+            t.send(new HitBuilders.ExceptionBuilder()
+                    .setDescription(
+                            new StandardExceptionParser(this, null)
+                                    .getDescription(Thread.currentThread().getName(), e))
+                    .setFatal(false)
+                    .build()
+            );
+        }
+    }
+
+    /***
+     * Tracking event
+     *
+     * @param category event category
+     * @param action   action of the event
+     * @param label    label
+     */
+    public void trackEvent(String category, String action, String label) {
+        Tracker t = getGoogleAnalyticsTracker();
+
+        // Build and send an Event.
+        t.send(new HitBuilders.EventBuilder().setCategory(category).setAction(action).setLabel(label).build());
+    }
+    // endregion Analytics
 }
